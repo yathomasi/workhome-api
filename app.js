9b2bc06fdba1d5cccf58505b2dbf2b47aefe7c7c
Thomas Kunwar <yathomasi@gmail.com>
2019-12-14 17:18:17 +0545

    adds morgan logger

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require('morgan');
 
 require("dotenv").config();
 
 const port = process.env.PORT || 4000;
 const app = express();
 
+if (process.env.NODE_ENV == 'development') {
+  app.use(logger('dev'));
+  console.log("Development mode on");
+}
 mongoose.connect(
   process.env.MONGODB_URL, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    useCreateIndex:true
+    useCreateIndex: true
   }
 );
 const db = mongoose.connection;
@@ -23,6 +28,7 @@ const indexRouter = require("./routes/index")
 const userRouter = require("./routes/user")
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/',indexRouter)
 app.use('/test',testRouter)
 app.use('/user',userRouter)
@@ -30,3 +36,5 @@ app.use('/user',userRouter)
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+module.exports = app;
