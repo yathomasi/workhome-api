bf65c4bb48b4c7d2e7311f9e23c6f87d8182e5f3
Thomas Kunwar <yathomasi@gmail.com>
2019-12-14 17:24:34 +0545

    update user schema

--- models/user.js
+++ models/user.js
@@ -1,20 +1,51 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 const bcrypt = require('bcryptjs')
 let SALT = 14
 
 const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        lowercase: true,
+        required: [true, "username can't be blank"],
+        match: [/^[a-zA-Z0-9]+$/, 'username is invalid'],
+        unique: true,
+        index: true
+    },
     email: {
         type: String,
-        required: true,
-        unique: 1,
-        trim: true
+        required: [true, "email can't be blank"],
+        match: [/\S+@\S+\.\S+/, 'email is invalid'],
+        unique: true,
+        index: true,
+        maxlength: 255
     },
     password: {
         type: String,
         required: true,
-        minlength: 6
-    }
+        minlength: 6,
+        maxlength: 255
+    },
+    name: {
+        type: String,
+        minlength: 3,
+        maxlength: 50
+    },
+    bio: {
+        type: String,
+        minlength: 3,
+        maxlength: 50
+    },
+    isAdmin: Boolean
+
+}, {
+    timestamps: true
 });
+
+userSchema.plugin(uniqueValidator, {
+    message: 'is already taken.'
+})
+
 userSchema.pre('save', function (next) {
     let user = this;
     if (user.isModified('password')) {
